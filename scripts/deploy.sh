9fda5bc8f6c4c3465f728dcfb552cdfecc5ae0e2
Emanuel Felipe Batista de Oliveira <emanuel@zgsolucoes.com.br>
2019-03-12 14:38:18 -0300

    Alterando logica do build

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-wget -o nxfilter.txt http://www.nxfilter.org/changelog/nxfilter.txt
-
 versoes=$(grep -oP '(?<=^[0-9]{4}\/[0-9]{2}\/[0-9]{2}, v)(4.3.*)' nxfilter.txt)
 
 for versao in ${versoes}; do
 	docker build --build-arg "NXFILTER_VERSION=${versao}" -t emanuelflp/nxfilter:${versao} .
+	docker push emanuelflp/nxfilter:${versao}
 done
