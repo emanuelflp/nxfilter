b37a838842839cd24538f0eb2d3f42856902a2f6
Emanuel Felipe Batista de Oliveira <emanuel@zgsolucoes.com.br>
2019-03-12 15:17:28 -0300

    Alterando URL de captura do pacote

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@
 
 curl --output nxfilter.txt "http://www.nxfilter.org/changelog/nxfilter.txt"
 
-versoes=$(grep -oP '(?<=^[0-9]{4}\/[0-9]{2}\/[0-9]{2}, v)(4.3.*)' nxfilter.txt)
+versoes=$(grep -oP '(?<=^[0-9]{4}\/[0-9]{2}\/[0-9]{2}, v)(4.3.*)' nxfilter.txt | sort -n)
 
 for versao in ${versoes}; do
 	echo "${versao}"
