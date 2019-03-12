d762f6c42f98cbaebf8b60babb7161b1727b1dec
Emanuel Felipe Batista de Oliveira <emanuel@zgsolucoes.com.br>
2019-03-12 14:40:44 -0300

    renomeando pasta de script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+curl --output nxfilter.txt "http://www.nxfilter.org/changelog/nxfilter.txt"
+
 versoes=$(grep -oP '(?<=^[0-9]{4}\/[0-9]{2}\/[0-9]{2}, v)(4.3.*)' nxfilter.txt)
 
 for versao in ${versoes}; do
